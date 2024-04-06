aa968d1c9b003cdf0320ee2201b906ba0466f047
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-06 14:10:23 -0400

    NavBar added

---
+++ demo/src/main/react/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+import ReactDom from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <AppContainer></AppContainer>,
+      children: [
+        {
+          index: true,
+          element: <div> Empty Index</div>
+        },
+      ]
+    },
+  ])
+  
+  ReactDom.createRoot(document.getElementById('app')).render(
+    <React.StrictMode>
+        <RouterProvider router = {router} />
+    </React.StrictMode>
+  )
