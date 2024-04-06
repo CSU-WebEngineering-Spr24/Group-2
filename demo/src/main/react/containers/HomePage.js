fffbe27feb1141dab1270c95f90a431c3f798a35
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-06 14:16:19 -0400

    more Mods

---
+++ demo/src/main/react/containers/HomePage.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Nav from './components/nav/Nav';
+import { Outlet } from 'react-router-dom';
+const AppContainer = () => {
+
+
+
+
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      
+    </>
+  )
+}
+export default HomePage
