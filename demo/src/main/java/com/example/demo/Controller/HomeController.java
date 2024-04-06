2813be2ad8a74cfbdbc9fd371c2614b29ed14e67
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-06 14:02:02 -0400

    Commit

---
+++ demo/src/main/java/com/example/demo/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    //  @GetMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    @GetMapping(value = {"/", "/currentxkcdcomic", "/pastxkcdcomic", "/nasadate", "/nasacount", "/example", "/nasadaterange"})
+    public String index(){
+        return "index";
+    }
+}
