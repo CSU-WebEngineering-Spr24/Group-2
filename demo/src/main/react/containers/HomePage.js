7db921436d2beee068490163b41b7d982cd41e39
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-06 14:55:10 -0400

    push npx web build added

--- demo/src/main/react/containers/HomePage.js
+++ demo/src/main/react/containers/HomePage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Nav from './components/nav/Nav';
 import { Outlet } from 'react-router-dom';
-const AppContainer = () => {
+const HomePage = () => {
 
 
 
