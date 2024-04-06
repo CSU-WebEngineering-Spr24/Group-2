53d2b836caf8f438e24d8dc4f34e61865dfbeed4
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-06 14:10:40 -0400

    commit

--- demo/src/main/react/containers/components/nav.js
+++ demo/src/main/react/containers/components/nav.js
@@ -1,15 +1,44 @@
-package com.example.demo.Controller;
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+const Nav = () => {
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
 
-    //  @GetMapping(value = {"/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
-    @GetMapping(value = {"/", "/Cards","/CardSet"})
-    public String index(){
-        return "index";
-        //some sample code more
-    }
+
+  return (
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          Web Dev Example
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="currentxkcdcomic">Current Comic</a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="nasadate"> Nasa Date </a>
+            </li>
+            <li class="nav-item">
+                <a class = "nav-link" href="nasacount">Nasa Count</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="pastxkcdcomic" >Past Comic</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="nasadaterange" >NasaDateRange</a>
+            </li>
+            
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  )
 }
+export default Nav
