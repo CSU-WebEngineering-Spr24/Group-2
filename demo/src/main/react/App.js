b3b03e608476037683c91dbc4f659a29312ca18e
jlassiter0522 <156385288+jlassiter0522@users.noreply.github.com>
2024-04-07 12:06:25 -0400

    Update App.js
    Add comments on error currently being experienced

--- demo/src/main/react/App.js
+++ demo/src/main/react/App.js
@@ -3,7 +3,7 @@ import ReactDom from "react-dom/client";
 import AppContainer from "/containers/AppContainer"
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-
+//Taken from my original spring demo NASA API, however, will not work or display a webpage. Results in a white label error unsure how to proceed will talk with the professor.
 const router = createBrowserRouter([
   {
     path:"/",
