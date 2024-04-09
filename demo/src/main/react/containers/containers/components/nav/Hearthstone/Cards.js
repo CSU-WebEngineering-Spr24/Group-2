13d11f6cb432f7f529ec9f8700434c27641041fc
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-09 17:49:43 -0400

    commit

--- demo/src/main/react/containers/containers/components/nav/Hearthstone/Cards.js
+++ demo/src/main/react/containers/containers/components/nav/Hearthstone/Cards.js
@@ -1,16 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios';
-import Nav from '../compenets/nav/Nav'
 const Cards = () =>{
     return(
-        <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="..."> </img>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <h1>Hi</h1>
     )
 }
 export default Cards
