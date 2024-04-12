13545c6a682acb000cfdb0ede919cd3603c4b6f8
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-12 15:42:54 -0400

    Commit

--- demo/src/main/react/containers/containers/components/nav/Hearthstone/Home.js
+++ demo/src/main/react/containers/containers/components/nav/Hearthstone/Home.js
@@ -2,7 +2,13 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 const Home = () =>{
     return(
-      <h1> Hi </h1>
+      <>
+        <h1> Hearthstone Card Search
+        </h1>
+        <p id="Header"> </p>
+
+      </>
+
     )
 }
 export default Home
