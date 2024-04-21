598775b71319fbf3abe553b8970e452cb736bb82
jlassiter0522 <jlassiter0522@gmail.com>
2024-04-21 17:15:13 -0400

    Commit

--- demo/src/main/react/containers/containers/components/nav/Hearthstone/Cards.js
+++ demo/src/main/react/containers/containers/components/nav/Hearthstone/Cards.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 const Cards = () =>{
     return(
-      <h1>Hi</h1>
+      <h1>Hello</h1>
     )
 }
 export default Cards
--- demo/src/main/react/containers/containers/components/nav/Hearthstone/Sets.js
+++ demo/src/main/react/containers/containers/components/nav/Hearthstone/Sets.js
@@ -2,7 +2,12 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios';
 const Sets = () =>{
     return(
-      <h1> Hi </h1>
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
     )
 }
 export default Sets
